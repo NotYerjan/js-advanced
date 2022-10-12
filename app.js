347a2015b3a01d741613b751941f62ac231ea275
Yerjan <yerjan@proton.me>
2022-10-12 16:29:09 +0300

    ex64

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-const person = {
-  id: 1,
-  firstName: "Mario",
-  lastName: "Rossi",
-  age: 25,
-};
-
-const { id, firstName: name, lastName: surname, age: old } = person;
-console.log(id, name, surname, old);
+let numberStore = [0, 1, 2];
+let newNumber = 3;
+numberStore = [...numberStore, newNumber];
+console.log(numberStore);
