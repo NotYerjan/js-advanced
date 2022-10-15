8fb108d1896aa4838fe9ea797660fa1edf75beb1
Yerjan <yerjan@proton.me>
2022-10-15 21:57:17 +0300

    ex74 done

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 /*
-Write a function called `printAsyncName`, which will take as parameters a callback and a string (which will be name). The callback function will simply print "Hello".
-The `printAsyncName` function will have to execute the callback function after an interval of 1 second. After an interval of 2 seconds, we must print the name that we take as a parameter.
+Write a function called `repeatHello`, which will take as parameters a callback.
+The callback function will simply print "Hello".
+The `repeatHello` function will have to execute the callback function with an interval of 1 second.
 
 Tips:
 
-- The `setTimeout` method will be useful
+- The `setInterval` method will be useful
 - The callback function must be an arrow function, can you also explain why?
 */
-function printAsyncName(callback, name) {
-  setTimeout(() => callback(), 1000);
-  setTimeout(() => console.log(name), 2000);
+function repeatHello(callback) {
+  let id = setInterval(() => callback(), 1000);
+  setTimeout(() => clearInterval(id), 5000);
 }
-printAsyncName(() => console.log("Hello"), "Yerjan");
+
+repeatHello(() => console.log("Hello"));
