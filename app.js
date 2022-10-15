b5c00926a4cfbee351dedd0765695f3fa785db21
Yerjan <yerjan@proton.me>
2022-10-15 22:44:21 +0300

    ex77

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 /*
-Write a promise. If the variable called number is greater than 10, then it returns a resolve, otherwise it returns a reject.
-Then manage the promise with then and catch in case of any reject.
-
-Tips:
-In then you have to print the value of number
-In catch you have to print the error
+Write a first promise that takes as parameter the variable isLogged.
+If the variable is true, then we return a random number from the resolve,
+otherwise we dispatch an error. We write a second promise that takes a variable
+of type number as a parameter. If the input parameter is greater than 0.5,
+in the resolve we must return the following data: {name: "John", age: 24},
+otherwise we must dispatch an error. Once this is done, try to chain the promises
+to eventually return the final object {name: "John", age: 24}
 */
-const number = 15;
+const isLogged = true;
 
-let isGreaterThanTen = new Promise((resolve, reject) => {
-  if (number > 10) {
-    return resolve(number);
-  }
-  return reject("ERROR!!! The number is less than or equal to 10");
-});
+function firstPromise(isLogged) {
+  return new Promise((res, rej) => {
+    if (isLogged) {
+      res(Math.random());
+    }
+    rej("You are not logged in!");
+  });
+}
 
-isGreaterThanTen
+function secondPromise(number) {
+  return new Promise((res, rej) => {
+    if (number > 0.5) {
+      res({ name: "John", age: 24 });
+    }
+    rej("Ooops!!! Something went wrong.");
+  });
+}
+
+firstPromise(isLogged)
+  .then((res) => secondPromise(res))
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
