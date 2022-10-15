513ea030d070d08c2ae6f0a436cebaa7e9a35bc9
Yerjan <yerjan@proton.me>
2022-10-15 23:10:38 +0300

    ex79

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
 /*
-Starting from the previous exercise, try to better handle all errors through
-the Error class and the catch method. Also add the finally method.
+In the exercise.js file, we find a user object. Write a function that saves the user object in localStorage with its 'user' key.
 */
-const isLogged = true;
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
 
-function firstPromise(isLogged) {
-  return new Promise((res, rej) => {
-    if (isLogged) {
-      res(Math.random());
-    }
-    rej(new Error("You are not logged in!"));
-  });
-}
-
-function secondPromise(number) {
-  return new Promise((res, rej) => {
-    if (number > 0.5) {
-      res({ name: "John", age: 24 });
-    }
-    rej(new Error("Ooops!!! Something went wrong."));
-  });
-}
-
-firstPromise(isLogged)
-  .then((res) => secondPromise(res))
-  .then((res) => console.log(res))
-  .catch((err) => console.log(err))
-  .finally(() => console.log("fetching is finished"));
+localStorage.setItem("user", user);
