85f69ac498787795ff220e94a766867869b1c6a9
Yerjan <yerjan@proton.me>
2022-10-15 15:27:58 +0300

    ex65 done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let numberStore = [0, 1, 2];
-let newNumber = 3;
-numberStore = [...numberStore, newNumber];
-console.log(numberStore);
+function sum(arr) {
+  return arr.reduce((a, b) => a + b);
+}
+let numArr = [1, 2, 3, 4, 5];
+console.log(sum(numArr));
