951554ec639c9ae0366ac087b0f19ec2249e0a96
Yerjan <yerjan@proton.me>
2022-10-15 20:35:15 +0300

    ex73

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 /*
-Perform a code refactoring by using the template literals.
+Write a function called `printAsyncName`, which will take as parameters a callback and a string (which will be name). The callback function will simply print "Hello".
+The `printAsyncName` function will have to execute the callback function after an interval of 1 second. After an interval of 2 seconds, we must print the name that we take as a parameter.
+
+Tips:
+
+- The `setTimeout` method will be useful
+- The callback function must be an arrow function, can you also explain why?
 */
-
-class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
+function printAsyncName(callback, name) {
+  setTimeout(() => callback(), 1000);
+  setTimeout(() => console.log(name), 2000);
 }
-
-const developer = new Person("Mario", "Rossi");
-console.log(`${developer.firstName} ${developer.lastName}`);
+printAsyncName(() => console.log("Hello"), "Yerjan");
