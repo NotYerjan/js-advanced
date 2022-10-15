a73feec2da02dabe437e1954aaba87ac270000b9
Yerjan <yerjan@proton.me>
2022-10-15 23:52:32 +0300

    ex83

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 /*
-Get the value of the form-input text input field and print it in the console.
+Get the value of all input and label fields.
+
+The output in the console must be:
+
+First Name: Mario
+Last Name: Rossi
+Age: 25
 */
 let formInputs = document.querySelectorAll(".form-input");
 for (let formInput of formInputs) {
-  console.log(formInput.value);
+  console.log(formInput.previousElementSibling.innerHTML, formInput.value);
 }
 // console.log(formInputs);
