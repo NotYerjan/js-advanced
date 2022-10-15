75f97a0befb9003c4a3bd39e93f6997eefe3709b
Yerjan <yerjan@proton.me>
2022-10-15 23:27:16 +0300

    ex81

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 /*
-Starting from the previous exercise, write a method that recovers the data saved in localStorage and prints them in the console.
+Get the value of the firstName text input field and print it in the console.
 */
-const user = {
-  id: 1,
-  name: "John",
-  age: 25,
-};
-
-localStorage.setItem("user", JSON.stringify(user));
-localStorage
-  .getItem("user")
-  .then((res) => JSON.parse(res))
-  .then((user) => console.log(user))
-  .catch((err) => console.log(err));
+let firstName = document.getElementById("firstName").value;
+console.log(firstName);
